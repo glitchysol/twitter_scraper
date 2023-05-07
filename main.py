dea8af63ed76547656ba130f92045a4f9f1d4708
Devin Means <122479416+glitchysol@users.noreply.github.com>
2023-05-06 20:46:32 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "python"
+tweets = []
+limit = 100
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.user.username, tweet.rawContent])
+
+df = pd.DataFrame(tweets, columns=["Date", "User", "Tweet"])
+print(df)
